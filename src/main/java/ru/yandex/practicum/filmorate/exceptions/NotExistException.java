ac7a8e30926f8d6d11bd27fcb3cce4666dcf6cb0
Anastasia <119016195+missfury@users.noreply.github.com>
2023-05-01 18:40:01 +0300

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NotExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class NotExistException extends RuntimeException {
-    public NotExistException(String message) {
-        super(message);
-    }
-}
