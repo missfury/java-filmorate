5c7b825927c289ca8fccb2d2446241b3a772a95a
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-05-01 17:40:26 +0300

    feat: Добавить классы и контроллеры для фильмов и пользователей

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private int id;
+    @NotBlank(message = "Укажите название фильма")
+    private String name;
+    @Size(max = 200, message = "Максимальная длина описания 200 символов")
+    private String description;
+    @NotNull(message = "Укажите дату выхода фильма")
+    private LocalDate releaseDate;
+    @NotNull(message = "Укажите продолжительность фильма")
+    @PositiveOrZero(message = "Продолжительность фильма не может быть отрицательной")
+    private Integer duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validate/UserValidate.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.validate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+
+@Slf4j
+public class UserValidate {
+    public static void validate(@Valid @RequestBody User user) {
+            if (user.getLogin().contains(" ")){
+            log.warn("В логине содержатся пробелы, логин: {}", user.getLogin());
+            throw new ValidationException("Логин содержит пробел");
+        }
+        if (user.getName() == null || user.getName().isEmpty()){
+            user.setName(user.getLogin());
+            log.warn("Имя пользователя не заполнено. В качестве имени пользователя используется логин: {}",
+            user.getLogin());
+        }
+    }
+}
