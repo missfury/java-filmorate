e3297e13444ff7c5d5a1d441ad09e94b445b962d
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-05-01 18:04:16 +0300

    fix: Исправить недочеты

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,13 +11,13 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private int id;
-    @NotBlank(message = "Укажите название фильма")
+    @NotBlank(message = "Укажите название фильма.")
     private String name;
-    @Size(max = 200, message = "Максимальная длина описания 200 символов")
+    @Size(max = 200, message = "Максимальная длина описания 200 символов.")
     private String description;
-    @NotNull(message = "Укажите дату выхода фильма")
+    @NotNull(message = "Укажите дату выхода фильма.")
     private LocalDate releaseDate;
-    @NotNull(message = "Укажите продолжительность фильма")
-    @PositiveOrZero(message = "Продолжительность фильма не может быть отрицательной")
+    @NotNull(message = "Укажите продолжительность фильма.")
+    @PositiveOrZero(message = "Продолжительность фильма не может быть отрицательной.")
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -11,13 +11,13 @@ import java.time.LocalDate;
 @Data
 public class User {
     private int id;
-    @Email(message = "Адрес электронной почты не соответствует формату")
-    @NotBlank(message = "Укажите адрес электронной почты")
+    @Email(message = "Адрес электронной почты не соответствует формату.")
+    @NotBlank(message = "Укажите адрес электронной почты.")
     private String email;
-    @NotBlank(message = "Укажите логин")
+    @NotBlank(message = "Укажите логин.")
     private String login;
     private String name;
-    @PastOrPresent(message = "Дата рождения не может быть в будущем")
-    @NotNull(message = "Укажите дату рождения")
+    @PastOrPresent(message = "Дата рождения не может быть в будущем.")
+    @NotNull(message = "Укажите дату рождения.")
     private LocalDate birthday;
 }
