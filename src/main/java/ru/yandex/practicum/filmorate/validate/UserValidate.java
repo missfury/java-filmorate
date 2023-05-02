5bc788201ad854e12c1703c4f282027f1efed5aa
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-05-02 13:03:59 +0300

    Исправить недочеты

--- src/main/java/ru/yandex/practicum/filmorate/validate/UserValidate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validate/UserValidate.java
@@ -2,18 +2,24 @@ package ru.yandex.practicum.filmorate.validate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
+import java.time.LocalDate;
+
 @Slf4j
-public class UserValidate{
-    public static void validate(@Valid @RequestBody User user){if (user.getLogin().contains(" ")){log.warn
-            ("В логине содержатся пробелы, логин: {}", user.getLogin());
-            throw new ValidationException("Логин содержит пробел");}
-        if (user.getName() == null || user.getName().isEmpty()){user.setName(user.getLogin());
-            log.warn("Имя пользователя не заполнено. В качестве имени пользователя используется логин: {}",
-            user.getLogin());}
+public class UserValidate {
+    public static void validate(@Valid @RequestBody User user) {
+        if (user.getLogin().contains(" ")) {
+            log.warn("В логине содержатся пробелы, логин: {}", user.getLogin());
+            throw new ValidationException("Логин содержит пробел");
+        }
+        if (user.getName() == null || user.getName().isEmpty()) {
+            log.warn("Имя пользователя не заполнено. Вместо имени пользователя используется логин: {}", user.getLogin());
+        }
     }
 }
+
