8c9fd5738acb543fce98dc1aea1ccf800fca7253
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-06-09 01:37:48 +0300

    fix: Исправить недочеты

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -118,21 +118,21 @@ public class UserDbStorage implements UserStorage {
                 friendId);
     }
 
-    private List<User> getFriendById(int id){
+    private List<User> getFriendById(int id) {
         return jdbcTemplate.query(
                 "SELECT * FROM users WHERE id IN (SELECT friend_id FROM friends WHERE user_id = ?)",
                 this::makeUser,
                 id);
     }
 
-    private User makeUser(ResultSet resultSet, int rowNum) throws SQLException{
+    private User makeUser(ResultSet resultSet, int rowNum) throws SQLException {
         int id = resultSet.getInt("id");
         String email = resultSet.getString("email");
         String login = resultSet.getString("login");
         String name = resultSet.getString("name");
         LocalDate birthday = resultSet.getDate("birthday").toLocalDate();
         Set<Integer> friends = new HashSet<>();
-        for (User user : getFriendById(id)){
+        for (User user : getFriendById(id)) {
             friends.add(user.getId());
         }
         return new User(id, email, login, name, birthday, friends);
