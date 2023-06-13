77f40b819a1e55f6abde8d3620eef9e70e3f7711
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-06-13 12:28:19 +0300

    fix: After review 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -1,13 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exceptions.NotExistException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
@@ -22,12 +22,18 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
-@Component
-@RequiredArgsConstructor
+@Repository("filmDbStorage")
 
 public class FilmDbStorage implements FilmStorage {
     private final JdbcTemplate jdbcTemplate;
     private final GenreStorage genreStorage;
+
+    @Autowired
+    public FilmDbStorage(JdbcTemplate jdbcTemplate, GenreStorage genreStorage) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.genreStorage = genreStorage;
+    }
+
 
     @Override
     public List<Film> getFilms() {
