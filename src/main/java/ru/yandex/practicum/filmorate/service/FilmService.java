4346b59669f930d96aa2928d259a00f4bc655110
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-06-13 15:20:44 +0300

    fix: After review 2

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,7 +1,8 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -14,11 +15,17 @@ import java.util.List;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 
 public class FilmService {
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
+
+    @Autowired
+    public FilmService(@Qualifier("filmDbStorage") FilmStorage filmStorage,
+                       @Qualifier("userDbStorage") UserStorage userStorage) {
+        this.filmStorage = filmStorage;
+        this.userStorage = userStorage;
+    }
 
     public List<Film> getFilms() {
         return filmStorage.getFilms();
