e944ed950bbe5c587dcc6880212664dcf4fbf480
AnastasiaKolycheva <tainstvennaya-dama@yandex.ru>
2023-06-14 13:39:56 +0300

    fix: After review 3

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -31,17 +31,6 @@ public class Film {
     private int rating;
     private LinkedHashSet<Genre> genres;
 
-    public Film(Integer id, String name, String description, LocalDate releaseDate, Integer duration,
-                Mpa mpa, LinkedHashSet<Genre> genres) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.mpa = mpa;
-        this.genres = genres;
-    }
-
 
     public void addGenre(Genre genre) {
         if (genres == null) {
